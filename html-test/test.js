6ba95cec3f003b821172cf65b03eac028be59cee
sergpolly <sergpolly@gmail.com>
2020-03-23 22:05:22 -0400

    added different operations, further refactor

--- html-test/test.js
+++ html-test/test.js
@@ -1,12 +1,18 @@
 var AA = 10;
 var BB = 21;
-var main_sentence = ""
+var main_sentence = "";
 var encode_dict;
 var task_dict;
 
 var input_id = "input_sentence";
 var task_id = "sentence";
 var encode_id = "lettercodes";
+
+
+// which operations are selected:
+var op_ids = ["plus", "minus", "multiply"];
+
+
 
 var reA = /[^АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ]/g;
 // /^[аАбБвВгГдДеЕёЁжЖзЗиИйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩъЪыЫьЬэЭюЮяЯ]+$/
@@ -22,18 +28,6 @@ var alphabet = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л'
 // mygrid.setAttribute('id', 'sentgrid');
 // document.body.appendChild(mygrid);
 
-function arb_random(a,b) {
-  // will generate random var between
-  // a,b including boundaries:
-  uni_rand_01 = Math.random();
-  //  make sure b>a, a and b are integers...
-  a -= 1;
-  uni_rand_ab = (b-a)*uni_rand_01 + a;
-  // integer version
-  return Math.ceil(uni_rand_ab);
-}
-
-
 function encodeSentence() {
   var sentence, s_arr;
   // empty the dicts:
@@ -41,6 +35,21 @@ function encodeSentence() {
   task_dict = {};
   // Get the sentence ...
   sentence = document.getElementById(input_id).value;
+  if (sentence == "") {
+    alert("Ну введите же предложение, наконец!");
+    return;
+  }
+  //  check operations
+  var ops_todo = [];
+  for (op_id of op_ids){
+    if ( document.getElementById(op_id).checked ){
+      ops_todo.push(op_id);
+    }
+  }
+  if (ops_todo.length < 1) {
+    alert("Ну выберите хоть какую-нибудь операцию!");
+    return;
+  }
   // sanitize input a bit -> unfirom, cyrillic, etc:
   sentence = sentence.trim().toUpperCase();
   main_sentence = sentence;
@@ -56,26 +65,14 @@ function encodeSentence() {
   // the "result" of those tasks are unique ...
   var codes = new Array(su_arr.length);
   for (var i = 0; i < su_arr.length; i++) {
-    // keep going until we find a unique task: 
-    var task_found = false;
-    var attempt_counter = 0;
-    while (!task_found){
-      attempt_counter += 1;
-      var m1 = arb_random(AA,BB);
-      var m2 = arb_random(AA,BB);
-      //  is this task unique ? i.e. m1*m2 result:
-      if (codes.indexOf(m1*m2) > -1) {
-        task_found = false;
-        console.log("    failed: "+m1+"*"+m2+" = "+(m1*m2));
-      } else {
-        task_found = true;
-      }
-    }
-    //  turn rand[0,1] -> rand[a,b] ...
-    console.log(attempt_counter+" attempts: "+m1+"*"+m2+" = "+(m1*m2));
-    codes[i] = m1*m2;
-    task_dict[su_arr[i]] = m1+"*\<br \/\>"+m2+"\= \?";
-    encode_dict[su_arr[i]] = m1*m2;
+    let letter = su_arr[i];
+    let operation = choose(ops_todo);
+    let [a,b,res] = encode_letter(letter, operation, codes);
+    let opstr = op_to_string(operation,html=true);
+    // upack and record everything:
+    codes[i] = res;
+    task_dict[letter] = a+opstr+b+"= \?";
+    encode_dict[letter] = res;
   }
 
   console.log(task_dict);
@@ -111,9 +108,6 @@ function encodeSentence() {
 
   }
 
-  // clear the form on completion
-  document.getElementById(input_id).value = "";
-
 }
 
 
@@ -124,6 +118,14 @@ function generateTask(){
   if (newgrid.hasChildNodes()) {
       newgrid.innerHTML = '';
   }
+
+  if (main_sentence == "") {
+    alert("Ну введите же предложение, наконец!");
+    return;
+  }
+
+  // clear the input form ...
+  document.getElementById(input_id).value = "";
 
   // operate on a global variable - whatever
   // how can one pass JS function returns around
@@ -154,3 +156,96 @@ function generateTask(){
 
 // STR SPLITTING
 // STRING.split(/\s/);
+
+
+function arithmetic_apply(a,b,op) {
+  // apply arithmetic operation
+  switch (op) {
+    case "plus":
+      result = a + b;
+      break;
+    case "minus":
+      result = a - b;
+      break;
+    case "multiply":
+      result = a * b;
+      break;
+    default:
+      result = null;
+  }
+  return result;
+}
+
+
+function op_to_string(op,html=false) {
+  // print arithemtic operation nicely:
+  switch (op) {
+    case "plus":
+      opstr = "+";
+      break;
+    case "minus":
+      opstr = "-";
+      break;
+    case "multiply":
+      opstr = "*";
+      break;
+    default:
+      opstr = null;
+  }
+  // add break if html is selected:
+  if (html) {
+    return "<br />"+opstr;
+  } else {
+    return opstr;
+  }
+}
+
+
+
+
+function arb_random(a,b) {
+  // will generate random var between
+  // a,b including boundaries:
+  uni_rand_01 = Math.random();
+  //  make sure b>a, a and b are integers...
+  a -= 1;
+  uni_rand_ab = (b-a)*uni_rand_01 + a;
+  // integer version
+  return Math.ceil(uni_rand_ab);
+}
+
+
+// function to encode a letter with a given operation ...
+function encode_letter(letter, operation, used_codes) {
+  // keep encoding until good combination found:
+  var task_found = false;
+  var attempt_counter = 0;
+  while (!task_found){
+    attempt_counter += 1;
+    var a = arb_random(AA,BB);
+    var b = arb_random(AA,BB);
+    //  is this task/code unique ? positive ?
+    potential_code = arithmetic_apply(a,b,operation);
+    opstr = op_to_string(operation);
+    operation_str = ( a + opstr + b );
+    if (used_codes.indexOf(potential_code) > -1) {
+      task_found = false;
+      console.log("  failed: "+operation_str);
+    } else if (potential_code < 0) {
+      task_found = false;
+      console.log("  failed: "+operation_str);
+    } else {
+      task_found = true;
+    }
+  }
+  // we've got this - returning:
+  console.log(attempt_counter+" attempts: "+operation_str);
+  return [a,b,potential_code];
+}
+
+
+// random choice from an array - nothing builtin in JS
+function choose(choices) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
